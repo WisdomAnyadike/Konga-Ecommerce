60506268ae4eca70159e5c5e0e73e2fa44ebbe17
WisdomAnyadike <anyadikewisdom@icloud.com>
2023-12-24 20:33:00 +0100

    third deploy

--- konga/index.js
+++ konga/index.js
@@ -1,7 +1,6 @@
 let fetcher = async () => {
   const response = await fetch("https://dummyjson.com/products");
   const data = await response.json();
-
   const products = data.products;
   localStorage.setItem("products", JSON.stringify(products));
 };
@@ -11,45 +10,74 @@ fetcher();
 let fetchedData = JSON.parse(localStorage.getItem("products"));
 console.log(fetchedData);
 
-let todaysDeals = fetchedData.slice(0, 9);
+let numbs = [4, 7, 9];
+let nums = [23, 24, 25, 26, 27, 28, 29];
+let wholeNum = [10, 12];
+
+function randomCreator(num) {
+  let random = Math.floor(Math.random() * (num.length - 1));
+  return random;
+}
+
+let randomNum = Math.floor(Math.random() * (nums.length - 1));
+
+let todaysDeals = fetchedData.slice(
+  Math.floor(Math.random() * numbs[randomCreator(numbs)]),
+  wholeNum[randomCreator(wholeNum)]
+);
 console.log(todaysDeals);
 
 let dealsContent = document.getElementById("deals-content");
 
 function displaySlide(name, arr, flex, num, type) {
   arr.forEach((deal, i) => {
-    name.innerHTML += `<button id="${type}button${i}" value='${
+    name.innerHTML += `<button id="${
       deal.id
+    }" onclick="showProduct(event)" value='${
+      deal.price
     }'  class="border-0 ${flex} rounded deal${num} d-flex  justify-content-around">
          
-    <img width="100px" height="110px" style="border-radius:10px;" src='${
+    <img width="100px" id="${deal.id}" value='${
+      deal.price
+    }' height="110px" style="border-radius:10px;" src='${
       deal.images[0]
     }' alt="">
 
 
- <p class="${type}text d-flex align-items-start justify-content-start flex-column">
+ <p value='${deal.price}' id="${
+      deal.id
+    }"  class="${type}text d-flex align-items-start justify-content-start flex-column">
   <span class="mb-2 mt-3" >
 ${deal.title}
   </span>
 
-  <s style="font-size:12px; font-weight:300;">N${
-    (  deal.price + deal.price * (deal.discountPercentage / 100)).toFixed(2)
-  } </s>
+  <s id="${deal.id}" value='${
+      deal.price
+    }' style="font-size:12px; font-weight:300;">N${(
+      deal.price +
+      deal.price * (deal.discountPercentage / 100)
+    ).toFixed(2)} </s>
  
-  <b class="d-flex mb-2 align-items-center justify-content-between w-100 "style="font-size:12px;> 
+  <b id="${deal.id}" value='${
+      deal.price
+    }' class="d-flex mb-2 align-items-center justify-content-between w-100 "style="font-size:12px;> 
   <legend style="font-size:10px;">  N${deal.price}.00  </legend> 
      
- <small class="danger" style="font-size:8px; color:#ff706d;"> -${
-   deal.discountPercentage
- }%</small>
+ <small id="${deal.id}" value='${
+      deal.price
+    }' class="danger" style="font-size:8px; color:#ff706d;"> -${
+      deal.discountPercentage
+    }%</small>
   </b>
 
  
 
-  <b style="font-size:11px; color:#32b27a;"> You saved N${(
-    deal.price *
-    (deal.discountPercentage / 100)
-  ).toFixed(2)} </b>
+  <b id="${deal.id}" value='${
+      deal.price
+    }' style="font-size:11px; color:#32b27a;"> You saved N${(
+      deal.price *
+      (deal.discountPercentage / 100)
+    ).toFixed(2)} </b>
 
 
  </p>
@@ -64,35 +92,28 @@ let sponsoredArr = fetchedData.slice(23, 29);
 console.log(sponsoredArr);
 let sponsoredContent = document.getElementById("sponsored-content");
 displaySlide(sponsoredContent, sponsoredArr, "flex-column", 2, "sponsored");
-let recommendedArr = fetchedData.slice(17, 23);
+
+let recommendedArr = fetchedData.slice(20, nums[randomNum]);
 let recommendedContent = document.getElementById("recommended-content");
 displaySlide(recommendedContent, recommendedArr, "flex-row", 3, "recommended");
 
+document.addEventListener("DOMContentLoaded", function () {
+  let navBar = document.getElementById("nav");
+  let searchBar = document.getElementById("search-bar");
+  let shoppingLine = document.getElementById("shopping-line");
+  window.addEventListener("scroll", () => {
+    let windowHeight = window.scrollY;
 
-document.addEventListener('DOMContentLoaded', function () {
-  let navBar = document.getElementById('nav')
-  let searchBar = document.getElementById('search-bar')
-  let shoppingLine = document.getElementById('shopping-line')
-window.addEventListener('scroll' , ()=> {  
-  
-
-  let windowHeight = window.scrollY
-
-   if(windowHeight > 100 ){
-          navBar.style.display = 'none'
-          searchBar.style.transition = '1s'
-          shoppingLine.style.transition = '1s'
-          searchBar.style.top = '0px'
-          shoppingLine.style.top = '60px'
-       } else if (windowHeight < 100 ){
-          navBar.style.display = 'flex'
-          searchBar.style.top = '40px'
-          shoppingLine.style.top = '100px'
-       }
-})  })
-
-function seeAllItems(){
-  window.location.href = 'seemore.html'
-
-}
-
+    if (windowHeight > 100) {
+      navBar.style.display = "none";
+      searchBar.style.transition = "1s";
+      shoppingLine.style.transition = "1s";
+      searchBar.style.top = "0px";
+      shoppingLine.style.top = "60px";
+    } else if (windowHeight < 100) {
+      navBar.style.display = "flex";
+      searchBar.style.top = "40px";
+      shoppingLine.style.top = "100px";
+    }
+  });
+});
--- konga/seemore.js
+++ konga/seemore.js
@@ -1,63 +1,85 @@
 let fetchedData = JSON.parse(localStorage.getItem("products"));
 console.log(fetchedData);
 
-let allItems = document.getElementById('allItems')
- fetchedData.forEach((data,i) => {
-allItems.innerHTML += `<button id="button${i}" value='${
-  data.id
-}' style="width:250px; height:400px; margin:auto; text-align:center;"  class="border-0 mb-2  flex-column align-items-center  rounded data${1} d-flex  justify-content-around">
+let allItems = document.getElementById("allItems");
+fetchedData.forEach((data, i) => {
+  allItems.innerHTML += ` <div class="d-flex mb-4 align-items-center bg-light flex-column" style="width:fit-content; height:fir-content;" > <button id="${
+    data.id
+  }" value='${
+    data.price
+  }' onclick="showProduct(event)" style="width:250px; height:fit-content; min-height:350px; margin:auto; text-align:center;"  class="border-0 mb-2  flex-column align-items-center  rounded data${1} d-flex  justify-content-around">
      
-<img width="85%" height="210px" style="border-radius:10px;" class="mt-2" src='${
-  data.images[0]
-}' alt="">
+<img id="${data.id}" value='${
+    data.price
+  }' width="85%" height="210px" style="border-radius:10px;" class="mt-2" src='${
+    data.images[0]
+  }' alt="">
 
 
-<p class="text d-flex align-items-center justify-content-start flex-column w-100">
+<p id="${data.id}" value='${
+    data.price
+  }' class="text d-flex align-items-center justify-content-start flex-column w-100">
 
-<span class="mb-2 mt-3 w-100  "  >
-${data.title}
+<span id="${data.id}" style="font-size:14px;"  value='${
+    data.price
+  }' class="mb-2 mt-3 w-100  "  >
+${data.title.toUpperCase()}
 </span>
 
 
 
-<b class="d-flex mb-2 align-items-center justify-content-between w-75 "style="font-size:12px;> 
+<b id="${data.id}" value='${
+    data.price
+  }' class="d-flex mb-2 align-items-center justify-content-between w-75 "style="font-size:12px;> 
 
-<legend style="font-size:10px;" class="ms-2 me-2">  N${data.price}.00  </legend> 
+<legend id="${data.id}" value='${
+    data.price
+  }' style="font-size:10px;" class="ms-2 me-2">  N${data.price}.00  </legend> 
 
-<s  class="ms-2 me-2" style="font-size:12px; font-weight:300;">N${
-    (  data.price + data.price * (data.discountPercentage / 100)).toFixed(2)
-    } </s>
+<s id="${data.id}" value='${
+    data.price
+  }'  class="ms-2 me-2" style="font-size:12px; font-weight:300;">N${(
+    data.price +
+    data.price * (data.discountPercentage / 100)
+  ).toFixed(2)} </s>
 
-<small class=" ms-2 me-2 danger" style="font-size:8px; color:#ff706d;"> -${
-data.discountPercentage
-}%</small>
+<small id="${data.id}" value='${
+    data.price
+  }' class=" ms-2 me-2 danger" style="font-size:8px; color:#ff706d;"> -${
+    data.discountPercentage
+  }%</small>
 
 </b>
 
+<div class="progress" role="progressbar" style="width:95%;" aria-label="Success example" aria-valuenow="${
+    data.stock
+  }" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar bg-success"  style="width:${data.stock}%;" >${
+    data.stock
+  }%</div>
+</div>
 
 
-<b onclick="addToCart(event)" onmouseenter="changeColor(event)"  style="font-size:14px;height:30px; width:95%; border:1px solid #f855a6; color:#f855a6;" class="d-flex align-items-center justify-content-center "> Add to cart </b>
 
 
 </p>
 
-</button>`
- })
+</button>
 
- function goHome(){
-    window.location.href = 'index.html'
-  
-  }
+<b onclick="addToCart(event)" id="${data.id}" value='${
+    data.price
+  }' onmouseenter="changeColor(event)"  style="font-size:14px;height:30px; width:250px; border:1px solid #f855a6; color:#f855a6;" class="d-flex align-items-center justify-content-center "> Add to cart </b> </div>
 
- 
+`;
+});
 
-  function changeColor(event) {
-    el = event.target
-    el.style.backgroundColor = '#f855a6'
-        el.style.color = 'white'
+function changeColor(event) {
+  el = event.target;
+  el.style.backgroundColor = "#f855a6";
+  el.style.color = "white";
 
-        el.addEventListener('mouseleave' , ()=>{
-            el.style.backgroundColor = 'white'
-            el.style.color = '#f855a6'
-        })
-  }
+  el.addEventListener("mouseleave", () => {
+    el.style.backgroundColor = "white";
+    el.style.color = "#f855a6";
+  });
+}
